a2843024874bf26b7aba5e05cd23bbe06eb3c0c3
Jakub Krok <50359069+Brtgkrk@users.noreply.github.com>
2023-03-03 02:01:49 +0100

    Mozliwosc zmiany danych zalogowanego uzytkownika

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -18,80 +18,85 @@ const HomePage: React.FC<HomePageProps> = ({ loggedInUser }) => {
     const [photos, setPhotos] = useState<Photo[]>([]);
     const [albums, setAlbums] = useState<Album[]>([]);
     const [users, setUsers] = useState<User[]>([]);
+    const [searchUserName, setSearchText] = useState('');
     const navigate = useNavigate();
 
-useEffect(() => {
-    const fetchPhotos = async () => {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/photos"
-        );
-        const data = await response.json();
-        setPhotos(data);
-    };
+    useEffect(() => {
+        const fetchPhotos = async () => {
+            const response = await fetch(
+                "https://jsonplaceholder.typicode.com/photos"
+            );
+            const data = await response.json();
+            setPhotos(data);
+        };
 
-    const fetchAlbums = async () => {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/albums"
-        );
-        const data = await response.json();
-        setAlbums(data);
-    };
+        const fetchAlbums = async () => {
+            const response = await fetch(
+                "https://jsonplaceholder.typicode.com/albums"
+            );
+            const data = await response.json();
+            setAlbums(data);
+        };
 
-    const fetchUsers = async () => {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const data = await response.json();
-        setUsers(data);
-      };
+        const fetchUsers = async () => {
+            const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+            );
+            const data = await response.json();
+            setUsers(data);
+        };
 
-    fetchPhotos();
-    fetchAlbums();
-    fetchUsers();
-}, []);
+        fetchPhotos();
+        fetchAlbums();
+        fetchUsers();
+    }, []);
 
-return (
-    <div>
-        <NavigationBar loggedInUser={loggedInUser}/>
-        <div>Home Page</div>
+    const handleSearchInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setSearchText(event?.target.value);
+    }
 
-        <div className="post-container">
-            {albums.map((album) => {
-                const user = users.find((user) => user.id === album.userId);
-                const albumPhotos = photos.filter((photo) => album.id === photo.albumId);
-                let maxPhotos = 10; // Liczba zdjec ograniczona aby przyspieszyc ladowanie aplikacji
-                return (
-                    <div 
-                        key={album.id} 
-                        className="post album-container" 
-                        onClick={() => navigate(`/album/${album.id}`)}
-                        >
-                        <div className="post-header">
-                            <div className="post-author">twórca: <b>{user?.name}</b></div>
+    const filteredUsers = users.filter(user => user.name.toLowerCase().includes(searchUserName.toLowerCase()));
+    const filteredAlbums = albums.filter(album => filteredUsers.some(user => user.id === album.userId));
+
+    return (
+        <div>
+            <NavigationBar loggedInUser={loggedInUser}/>
+            <div className="post-container">
+                <h2>Wszystkie albumy ze zdjęciami</h2>
+                <input className="" type="text" value={searchUserName} onChange={handleSearchInputChange} placeholder="Wyszukaj po twórcy" />
+                {filteredAlbums.map((album) => {
+                    const user = users.find((user) => user.id === album.userId);
+                    const albumPhotos = photos.filter((photo) => album.id === photo.albumId);
+                    let maxPhotos = 10; // Liczba zdjec ograniczona aby przyspieszyc ladowanie aplikacji
+                    return (
+                        <div 
+                            key={album.id}
+                            className="post album-container" 
+                            onClick={() => navigate(`/album/${album.id}`)}
+                            >
+                            <div className="post-header">
+                                <div className="post-author">twórca: <b>{user?.name}</b></div>
+                            </div>
+                            <div className="album-body">
+                                <div className="post-title">album: <b>{album.title}</b></div>
+                                    <div>{ 
+                                        albumPhotos.map((albumPhoto, index) => {
+                                            const maxPhotosLeft = maxPhotos - index;
+                                            return maxPhotosLeft > 0 && (
+                                                <div key={albumPhoto.id}>
+                                                    <img src={albumPhoto.thumbnailUrl} alt={albumPhoto.title} id="album-photo"/>
+                                                </div>
+                                            );
+                                        })
+                                    }</div>
+                            </div>
                         </div>
-                        <div className="album-body">
-                            <div className="post-title">album: <b>{album.title}</b></div>
-                            
-                                <div>{ 
-                                    albumPhotos.map((albumPhoto, index) => {
-                                        const maxPhotosLeft = maxPhotos - index;
-                                        return maxPhotosLeft > 0 && (
-                                            <div>
-                                                <img src={albumPhoto.thumbnailUrl} alt={albumPhoto.title} id="album-photo"/>
-                                            </div>
-                                        );
-                                    })
-                                }</div>
-                            
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
+
         </div>
-
-    </div>
-);
-
+    );
 }
 
 export default HomePage;
--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -8,150 +8,248 @@ import NavigationBar from "./NavigationBar";
 import { User } from "../types/User";
 import { Photo } from "../types/Photo";
 import { Album } from "../types/Album";
+import { Post } from "../types/Post";
 // CSS
 import "../styles/UserPage.css";
 import "../styles/PostPage.css";
-import { Post } from "../types/Post";
 
 type UserPageProps = {
     loggedInUser: string;
+    label?: string;
+    value?: string;
+    fieldType?: string;
+    onSave?: (newValue: string) => void;
 };
 
-const UserPage: React.FC<UserPageProps> = ({ loggedInUser }) => {
+const UserPage: React.FC<UserPageProps> = ({ 
+    loggedInUser, 
+    label,
+    value,
+    fieldType,
+    onSave
+    }) => {
+    const [currentUser, setCurrentUser] = useState<User>();
     const [albums, setAlbums] = useState<Album[]>([]);
-    const [photos, setPhotos] = useState<Photo[]>([]);
     const [posts, setPosts] = useState<Post[]>([]);
-    const [users, setUsers] = useState<User[]>([]);
+
     const navigate = useNavigate();
     const { username } = useParams();
 
-useEffect(() => {
-    const fetchUsers = async () => {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const data = await response.json();
-        setUsers(data);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedValue, setEditedValue] = useState(value);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const [changingFieldType, setChangingFieldType] = useState("");
+
+    useEffect(() => {
+        const fetchCurrentUser = async () => {
+            const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+            );
+            const data = await response.json();
+            setCurrentUser(data.find((user: { username: string | undefined; })=> user.username === username));
+        };
+
+        fetchCurrentUser();
+    }, []);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts"
+            );
+            const data: Post[] = await response.json();
+            const currentUserPosts = data.filter((post) => post.userId === currentUser?.id);
+            setPosts(currentUserPosts);
+        };
+
+        const fetchAlbums = async () => {
+            const response = await fetch(
+                "https://jsonplaceholder.typicode.com/albums"
+            );
+            const data: Album[] = await response.json();
+            const currentUserAlbums = data.filter((album) => album.userId === currentUser?.id);
+            setAlbums(currentUserAlbums);
+        };
+
+        fetchPosts();
+        fetchAlbums();
+    },[currentUser])
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEditedValue(event.target.value);
+    };
+    
+    const handleSaveClick = () => {
+        if (currentUser?.username != loggedInUser) return;
+        let isValid = true;
+        if (changingFieldType === "email") {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (editedValue)
+            isValid = emailRegex.test(editedValue);
+            if (!isValid) {
+            setErrorMessage("Wprowadź poprawny adres email");
+            }
+        } else if (changingFieldType === "phone") {
+            const phoneRegex = /^\d{9}$/;
+            if (editedValue)
+            isValid = phoneRegex.test(editedValue);
+            if (!isValid) {
+            setErrorMessage("Wprowadź poprawny numer telefonu (9 cyfr)");
+            }
+        } else if (changingFieldType === "website") {
+            const websiteRegex = /^[^\s]+\.[^\s]+$/;
+            if (editedValue)
+            isValid = websiteRegex.test(editedValue);
+            if (!isValid) {
+            setErrorMessage("Wprowadź poprawny adres strony internetowej");
+            }
+        } else if (changingFieldType === "zipcode") {
+            const zipcodeRegex = /^\d{2}-\d{3}$/;
+            if (editedValue)
+            isValid = zipcodeRegex.test(editedValue);
+            if (!isValid) {
+            setErrorMessage("Wprowadź poprawny kod pocztowy (xx-xxx)");
+            }
+        } else if (changingFieldType === "latitude" || changingFieldType === "longitude") {
+            const coordinateRegex = /^-?\d+(?:\.\d+)?$/;
+            if (editedValue)
+            isValid = coordinateRegex.test(editedValue);
+            if (!isValid) {
+            setErrorMessage(
+                "Wprowadź poprawną wartość współrzędnej (liczba zmiennoprzecinkowa)"
+            );
+            }
+        }
+        
+        if (isValid) {
+            if (changingFieldType === "email" && currentUser && editedValue) currentUser.email = editedValue;
+            if (changingFieldType === "username" && currentUser && editedValue) currentUser.username = editedValue;
+            if (changingFieldType === "phone" && currentUser && editedValue) currentUser.phone = editedValue;
+            if (changingFieldType === "website" && currentUser && editedValue) currentUser.website = editedValue;
+            setIsEditing(false);
+            setEditedValue(value);
+            setErrorMessage("");
+        }
+    };
+    
+    const handleCancelClick = () => {
+        setIsEditing(false);
+        setEditedValue(value);
+        setErrorMessage("");
     };
 
-    const fetchAlbums = async () => {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/albums"
-        );
-        const user = users.find((user) => user.username === username);
-        const data: Album[] = await response.json();
-        const currentUserAlbums = data.filter((album) => album.userId === user?.id);
-        setAlbums(data);
-        //console.log("all posts: " + data + "\n current user post: " + currentUserAlbums + "current user: " + user?.id + "all users " + users);
-    };
+    const handleDataClick = (type : string) => {
+        return () => {
+            setChangingFieldType(type);
+            console.log(`Zmiana wybranego pola na ${changingFieldType}`);
+            setIsEditing(true);
+        }
+    }
 
-    const fetchPhotos = async () => {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/photos"
-        );
-        const data = await response.json();
-        setPhotos(data);
-    };
+    //changingFieldType = "username";
 
-    fetchUsers();
-    fetchAlbums();
-    fetchPhotos();
-}, []);
-
-useEffect(() => {
-    const fetchPosts = async () => {
-        const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-        );
-        const user = users.find((user) => user.username === username);
-        console.log("current user is " + user + "\n all users: " + users);
-        const data: Post[] = await response.json();
-        const currentUserPosts = data.filter((post) => post.userId === user?.id);
-        setPosts(currentUserPosts);
-    };
-    fetchPosts();
-})
-
-const user = users.find((user) => user.username === username);
-
-return (
-    <div>
-        <NavigationBar loggedInUser={loggedInUser}/>
-        <div>Strona użytkownika: {user?.username}</div>
-
-        <div className="user-container">
-      <h1>{user?.name}</h1>
-      <div className="user-details">
+    return (
         <div>
-            <h2>Dane personalne:</h2>
-            <p>Username: {user?.username}</p>
-            <p>Email: {user?.email}</p>
-            <p>Phone: {user?.phone}</p>
-            <p>Website: {user?.website}</p>
-        </div>
-        <div>
-            <h2>Adres:</h2>
-            <p>Street: {user?.address.street}</p>
-            <p>Suite: {user?.address.suite}</p>
-            <p>City: {user?.address.city}</p>
-            <p>Zipcode: {user?.address.zipcode}</p>
-            <p>Latitude: {user?.address.geo.lat}</p>
-            <p>Longitude: {user?.address.geo.lng}</p>
-        </div>
-        <div>
-            <h2>Firma:</h2>
-            <p>Name: {user?.company.name}</p>
-            <p>Catchphrase: {user?.company.catchPhrase}</p>
-            <p>BS: {user?.company.bs}</p>
-        </div>
-            <div className="post-container">
-            <h2>Posty użytkownika {user?.username}:</h2>
-                {posts.map((post) => {
-                    //const user = users.find((user) => user.id === post.userId);
-                    return (
-                        <div key={post.id} className="post">
-                            <div className="post-header">
-                                <div className="post-author">{user?.name}</div>
-                            </div>
-                            <div className="post-body">
-                                <div className="post-title">{post.title}</div>
-                                <div className="post-text">{post.body}</div>
-                                <div className="post-comments">
-                                </div>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-            <h2>Albumy użytkownika {user?.username}:</h2>
-            <div className="post-container">
-            {albums.map((album) => {
-                const user = users.find((user) => user.id === album.userId);
-                const albumPhotos = photos.filter((photo) => album.id === photo.albumId);
-                return (
-                    <div 
-                        key={album.id} 
-                        className="post album-container" 
-                        onClick={() => navigate(`/album/${album.id}`)}
-                        >
-                        <div className="post-header">
-                            <div className="post-author">twórca: <b>{user?.name}</b></div>
-                        </div>
-                        <div className="album-body">
-                            <div className="post-title">album: <b>{album.title}</b></div>
-                            
-                        </div>
+            <NavigationBar loggedInUser={loggedInUser}/>
+            <div className="user-container">
+                <h1>{currentUser?.name}</h1>
+                {(currentUser?.username === loggedInUser) ? <div className="info-edit">
+                    <p className="p-green">
+                        istnieje możliwość edycji danych
+                    </p>
+                    <p className="p-green">
+                        Wystarczy kliknąć na dane do zmiany
+                    </p>
+                </div> : null}
+                <div className="user-details">
+
+
+                <label>{label}</label>
+                {isEditing ? (
+                    <div>
+                    <input
+                        type={changingFieldType === "email" ? "email" : "text"}
+                        value={editedValue}
+                        onChange={handleInputChange}
+                    />
+                    <button onClick={handleSaveClick} className="btn-save">Zapisz</button>
+                    <button onClick={handleCancelClick} className="btn-red">Anuluj</button>
+                    {errorMessage && <p>{errorMessage}</p>}
                     </div>
-                );
-            })}
-        </div>
-        </div>
-      </div>
+                ) : (
+                    <div>
+                    <span>{value}</span>
+                    </div>
+                )}
 
-      
-    </div>
-);
+                    <div>
+                        <h2>Dane personalne:</h2>
+                        <p onClick={handleDataClick("username")} className="p-user">Nazwa użytkownika: {currentUser?.username}</p>
+                        <p onClick={handleDataClick("email")} className="p-user">Email: {currentUser?.email}</p>
+                        <p onClick={handleDataClick("phone")} className="p-user">Telefon: {currentUser?.phone}</p>
+                        <p onClick={handleDataClick("website")} className="p-user">Strona: {currentUser?.website}</p>
+                    </div>
+                    <div>
+                        <h2>Adres:</h2>
+                        <p>Street: {currentUser?.address.street}</p>
+                        <p>Suite: {currentUser?.address.suite}</p>
+                        <p>City: {currentUser?.address.city}</p>
+                        <p>Zipcode: {currentUser?.address.zipcode}</p>
+                        <p>Latitude: {currentUser?.address.geo.lat}</p>
+                        <p>Longitude: {currentUser?.address.geo.lng}</p>
+                    </div>
+                    <div>
+                        <h2>Firma:</h2>
+                        <p>Name: {currentUser?.company.name}</p>
+                        <p>Catchphrase: {currentUser?.company.catchPhrase}</p>
+                        <p>BS: {currentUser?.company.bs}</p>
+                    </div>
 
+
+
+                    <div className="post-container">
+                        <h2>Posty użytkownika {currentUser?.username}:</h2>
+                            {posts.map((post) => {
+                                //const user = users.find((user) => user.id === post.userId);
+                                return (
+                                    <div key={post.id} className="post">
+                                        <div className="post-header">
+                                            <div className="post-author">{currentUser?.name}</div>
+                                        </div>
+                                        <div className="post-body">
+                                            <div className="post-title">{post.title}</div>
+                                            <div className="post-text">{post.body}</div>
+                                            <div className="post-comments">
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                    </div>
+                    <h2>Albumy użytkownika {currentUser?.username}:</h2>
+                    <div className="post-container">
+                        {albums.map((album) => {
+                            return (
+                                <div 
+                                    key={album.id} 
+                                    className="post album-container" 
+                                    onClick={() => navigate(`/album/${album.id}`)}
+                                    >
+                                    <div className="post-header">
+                                        <div className="post-author">twórca: <b>{currentUser?.name}</b></div>
+                                    </div>
+                                    <div className="album-body">
+                                        <div className="post-title">album: <b>{album.title}</b></div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default UserPage;
